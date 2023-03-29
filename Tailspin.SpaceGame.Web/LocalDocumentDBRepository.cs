17ea194c5ca2381d73182a7aab15e784edc6a1d4
Sachin Gowda <Sachin.Gowda@p360.com>
2023-03-29 10:20:59 -0400

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -54,15 +54,15 @@ namespace TailSpin.SpaceGame.Web
             Func<T, bool> queryPredicate,
             Func<T, int> orderDescendingPredicate,
             int page = 1, int pageSize = 10
-        )
-        {
-            var result = _items
+            )
+            {
+                var result = _items
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize - 1); // take items
+                .Take(pageSize); // take items
 
-            return Task<IEnumerable<T>>.FromResult(result);
+                return Task<IEnumerable<T>>.FromResult(result);
         }
 
         /// <summary>
